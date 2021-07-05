1dbc57122670ebe43f9c8ed2d0de59c46afd613e
paragdev441 <parag.sharmadev441@gmail.com>
2021-07-06 00:21:03 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ListBlock from "./containers/ListBlock";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <ListBlock />
+    </div>
+  );
+}
---
+++ src/apis/restaurents.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+axios.create({
+  baseURL: "https://api.npoint.io/93bed93a99df4c91044e"
+});
+
+export default axios;
